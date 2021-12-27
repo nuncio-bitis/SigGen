f6275ccff2eb97c8b377e9893ace142372e78871
Jim Parziale <james.p.parziale@gmail.com>
2021-12-27 10:08:35 -0500

    Add raw2wav

--- bin/raw2wav.sh
+++
@@ -1,8 +0,0 @@
-for f in $*
-do
-  echo
-  echo "--- $f"
-  name=$(basename -s .dat $f)
-  sox --multi-threaded --show-progress --bits 32 --channels 1 --encoding floating-point --rate 16000 --endian little -t raw "$f" "$name.wav"
-done
-echo
